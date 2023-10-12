0353cc98982eed868decd7f89011fe938a1a658f
moriongit <132233572+moriongit@users.noreply.github.com>
2023-10-13 01:33:20 +0400

    Add files via upload

---
+++ 12 october Homeowork/a+b/a+b/Program.cs
@@ -0,0 +1,25 @@
+﻿/*#region Birinci ededi ikinciye menimsetmek
+int firstNumber = 21;
+int secondNumber = 22;
+firstNumber = firstNumber + secondNumber;
+secondNumber = firstNumber - secondNumber;
+firstNumber = firstNumber - secondNumber;
+Console.WriteLine(firstNumber);
+Console.WriteLine(secondNumber);
+#endregion*/
+
+#region Fibonacci
+Console.WriteLine("Input your number:");
+int input = Convert.ToInt32(Console.ReadLine());
+int fibFirstNumber = 0;
+int fibSecondNumber = 1;
+int Fib = fibFirstNumber + fibSecondNumber;
+while (Fib <= input)
+{
+    Console.WriteLine(Fib);
+    fibFirstNumber = fibSecondNumber;
+    fibSecondNumber = Fib;
+    Fib = fibFirstNumber + fibSecondNumber;
+    
+}
+#endregion
