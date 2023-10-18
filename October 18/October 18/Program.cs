bb1df6a26ec62e4b87e0c17c0328d9874b94165b
moriongit <132233572+moriongit@users.noreply.github.com>
2023-10-18 23:19:26 +0400

    Add files via upload

---
+++ October 18/October 18/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace October_18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { -1, 2, -3, 4, -5 };
+            PosInt(array);
+        }
+        static void PosInt(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i]<0)
+                {
+                    array[i] = array[i]*-1;
+                }
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
