886d718dda653c1d269c856716b53bbe6bffe425
moriongit <132233572+moriongit@users.noreply.github.com>
2023-10-20 00:56:48 +0400

    Add files via upload

---
+++ October 19/October 19/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace October_19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User ID1 = new User();
+            ID1.UserName = "Bob";
+            ID1.Password = "password";
+            ID1.Login();
+            ID1.Login();
+            ID1.Logout();
+        }
+    }
+}
---
+++ October 19/October 19/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace October_19
+{
+    public class User
+    {
+        public string UserName;
+        public string Password;
+        public int Count = 0;
+
+
+        public void Login()
+        {
+            Console.WriteLine("User has successfully logged in");
+            Count++;
+            if (Count > 1)
+            {
+                Console.WriteLine("User has logged in before");
+            }
+            else if (Count == 1)
+            {
+                Console.WriteLine("User has never logged in before");
+
+            }
+
+        }
+        public void Logout()
+        {
+            Console.WriteLine("User has successfully logged out");
+
+        }
+    }
+}
