d6ce746817be55a578239cd7105ce8f327b9125b
moriongit <132233572+moriongit@users.noreply.github.com>
2023-11-02 01:30:07 +0400

    Add files via upload

---
+++ Indexer task 1/Indexer task 1/Models/Extension.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexer_task_1.Models
+{
+    public static class ExtensionMethods
+    {
+        public static bool IsPrime(this int number)
+        {
+            if (number <= 1)
+            {
+                return false;
+            }
+            if (number == 2)
+            {
+                return true;
+            }
+            if (number % 2 == 0)
+            {
+                return false;
+            }
+            for (int i = 3; i <= Math.Sqrt(number); i += 2)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static bool IsPowOfTwo(this int number)
+        {
+            if (number <= 0)
+            {
+                return false;
+            }
+            return (number & (number - 1)) == 0;
+        }
+    }
+}
---
+++ Indexer task 1/Indexer task 1/Program.cs
@@ -0,0 +1,16 @@
+﻿using Indexer_task_1.Models;
+
+namespace Indexer_task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            int num1 = 4;
+            Console.WriteLine(num1.IsPowOfTwo());
+            Console.WriteLine(num1.IsPrime());
+        }
+    }
+}
