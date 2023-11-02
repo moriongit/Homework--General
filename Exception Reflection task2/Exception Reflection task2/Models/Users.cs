f419fd5d1b946e44d6d650f0bba0b43f90fc6297
moriongit <132233572+moriongit@users.noreply.github.com>
2023-11-03 02:03:18 +0400

    Add files via upload

---
+++ Exception Reflection task2/Exception Reflection task2/Models/Users.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Reflection_task2.Models
+{
+    public class Users
+    {
+        private int _staticage;
+        private string _id;
+        private string _name;
+        
+
+        static void ChangeAge(int age)
+        {
+
+        }
+    }
+}
---
+++ Exception Reflection task2/Exception Reflection task2/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using Exception_Reflection_task2.Models;
+using System.Reflection;
+
+
+namespace Exception_Reflection_task2
+{
+    internal class program
+    {
+        static void Main()
+        {
+            Users s = new Users();
+            var Age = s.GetType().GetField("_staticage", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            Age.SetValue(s, 22);
+            Console.WriteLine(Age.GetValue(s));
+            
+            var Id = s.GetType().GetField("_id", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            Id.SetValue(s, "F201");
+            Console.WriteLine(Id.GetValue(s));
+            
+            var Name = s.GetType().GetField("_name", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            Name.SetValue(s, "Bob");
+            Console.WriteLine(Name.GetValue(s));
+        }
+    }
+}
