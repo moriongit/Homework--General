d05d155c51e2a75fa87f4864de3cc8fdd1054572
MorionGit <tempro.gmai@gmail.com>
2023-11-29 12:41:58 +0400

    AdminPanel

---
+++ AdminPanel/AdminPanel/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using AdminPanel.Helper;
+using AdminPanel.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AdminPanel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var dt = await SqlHelper.GetDatas("SELECT * FROM Worker");
+            return View(dt);
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
