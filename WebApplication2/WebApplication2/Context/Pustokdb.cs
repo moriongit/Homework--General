4ce4b3282905deef12f6a6218aaa96dc7ce19f61
MorionGit <tempro.gmai@gmail.com>
2023-12-01 13:22:20 +0400

    web

---
+++ WebApplication2/WebApplication2/Context/Pustokdb.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+using WebApplication2.Models;
+
+namespace WebApplication2.Context
+{
+    public class Pustokdb : DbContext
+    {
+        public DbSet<Slider>Sliders { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server = DESKTOP-OA1RSIR\SQLEXPRESS; Database = Pustok; Trusted_Connection = true");
+            
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using WebApplication2.Context;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            using Pustokdb context = new Pustokdb();
+            var sliders = await context.Sliders.ToListAsync();
+            return View(sliders);
+        }
+    }
+}
