b6b8f0618f7c0e2d2f8386d88295c3540a921122
MorionGit <tempro.gmai@gmail.com>
2023-12-05 12:56:00 +0400

    Blog, Author

---
+++ WebApplication2/WebApplication2/Models/Author.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication2.Models
+{
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public List<Blog> Blogs { get; set; }
+    }
+}
---
+++ WebApplication2/WebApplication2/Models/Blog.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication2.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Author Author { get; set; }
+        public DateTime CreatedAt { get; set; } 
+        public DateTime LastUpdatedAt { get; set; }
+        public int AuthorID { get; set; }
+        public bool? IsDeleted { get; set; }
+    }
+}
